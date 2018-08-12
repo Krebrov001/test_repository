2d602828c0d0106e2c01e70b892b7e339c7d72cc
Krebrov001 <krebrov001@student.butte.edu>
2018-08-12 12:11:19 -0700

    Adding interaction.
    Letting the user specify how many Fibonacci Numbers they want.

--- code1.cpp
+++ code1.cpp
@@ -3,7 +3,11 @@
 int Fib(int n);
 
 int main() {
-  std::cout << "First 10 Fibonacci Numbers: " << Fib(9) << std::endl;
+  int x;
+  
+  std::cout << "How many Fibonacci Numbers do you want? ";
+  std::cin  >> x;
+  std::cout << "First " << x << " Fibonacci Numbers: " << Fib(x - 1) << std::endl;
 }
 
 int Fib(int n) {
