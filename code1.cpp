345ac56aa953fef00e142ae65454710f5626fab8
Krebrov001 <krebrov001@student.butte.edu>
2018-08-12 11:48:21 -0700

    Added the function definition.
    The Fib() function computes the Fibonacci sequence recursively.

--- code1.cpp
+++ code1.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+int Fib(int n) {
+  if (n <= 1)
+    return n;
+  else
+    return Fib(n-1) + Fib(n-2);
+}
+
 int main() {
   std::cout << "First 10 Fibonacci Numbers: " << Fib(9) << std::endl;
 }
